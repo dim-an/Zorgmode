bdc7cb44e10a4fa9b4bdfcf8bf208d1f3ffd1c70
Dmitry Ermolov <epdmitry@yandex.ru>
2018-11-12 10:55:54 +0300

    Style

--- tests/test_archivation.py
+++ tests/test_archivation.py
@@ -10,6 +10,7 @@ from zorgtest import (
     ZorgTestCase,
 )
 
+
 class TestArchivation(ZorgTestCase):
     def test_simple_archivation(self):
         with tempfile.NamedTemporaryFile() as tmpf:
@@ -31,37 +32,40 @@ class TestArchivation(ZorgTestCase):
                 ("\n* Header 1\n"
                  "** Header 2\n"))
 
-
     def test_subheadline_archivation(self):
         with tempfile.NamedTemporaryFile() as tmpf:
             set_active_view_text(
-                ("#+ARCHIVE:{tempfile}\n"
+                "#+ARCHIVE:{tempfile}\n"
                 "* Header 1\n"
                 "** Header 2\n"
-                "* Header 3\n").format(tempfile=tmpf.name))
+                "* Header 3\n".format(tempfile=tmpf.name))
             set_active_view_cursor_position(3, 2)
             self.view.run_command("zorg_move_to_archive")
 
             self.assertEqual(
                 get_active_view_text(),
-                ("#+ARCHIVE:{tempfile}\n"
+                "#+ARCHIVE:{tempfile}\n"
                 "* Header 1\n"
-                "* Header 3\n").format(tempfile=tmpf.name))
+                "* Header 3\n".format(tempfile=tmpf.name)
+            )
             self.assertEqual(
                 tmpf.read().decode('utf-8'),
-                 "\n* Header 2\n")
+                "\n* Header 2\n"
+            )
 
-    def test_archivation_to_nonexisting_directory(self):
+    def test_archivation_to_nonexistent_file(self):
         set_active_view_text(
-            "#+ARCHIVE:/dev/null/inexistent_file\n"
+            "#+ARCHIVE:/dev/null/nonexistent_file\n"
             "* Header 1\n"
             "** Header 2\n"
-            "* Header 3\n")
+            "* Header 3\n"
+        )
         set_active_view_cursor_position(3, 2)
         self.view.run_command("zorg_move_to_archive", {"silent": True})
         self.assertEqual(
             get_active_view_text(),
-            ("#+ARCHIVE:/dev/null/inexistent_file\n"
+            "#+ARCHIVE:/dev/null/nonexistent_file\n"
             "* Header 1\n"
             "** Header 2\n"
-            "* Header 3\n"))
+            "* Header 3\n"
+        )
